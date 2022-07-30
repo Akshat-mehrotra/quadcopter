83eb9c92e83ca4f4cab158962c319ba8c838dfaa
akshat <akshatmehrotra2004@gmail.com>
2022-07-30 07:32:56 +0530

    Uploaded the lastest version
    Forgot to update the github files for quadcopter project

--- recognise.py
+++ recognise.py
@@ -1,78 +1,73 @@
 import numpy as np
 import cv2
 import math
-# multiple cascades: https://github.com/Itseez/opencv/tree/master/data/haarcascades
+from quadcopter import Drone
 
 face_cascade = cv2.CascadeClassifier('harrarface.xml')
 
 cap = cv2.VideoCapture(0)
 
-process = True
-base = 10
-delta = 10
-req_area = 24649
-threshold = 110
+req_area = 24649 #specific to my xml file
+threshold = 10
 
-actual_center = (int(base*round((cap.get(cv2.CAP_PROP_FRAME_WIDTH)//2)/base)), int(base*round((cap.get(cv2.CAP_PROP_FRAME_HEIGHT)//2)/base))) #(320, 240)
-print(actual_center)
+screen_width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+screen_height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+screen_center = (int(round(screen_width//2)), int(round(screen_height//2)))
 
-target_center = (0,0)
+print("Center OF screen is", screen_center)
 
-def move_z(rev=1):
-    print(3*rev)
 
-def move_y(rev=1, rapid=False):
-    print(2*rev)
+input("start?")
 
-def move_x(rev=1, rapid=False):
-    print(1*rev)
-
+drone = Drone()
+drone.calibrate()
 while 1:
     if process:
         ret, img = cap.read()
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray, 1.6, 5)
 
-        for (x,y,w,h) in faces:
+        for (x,y,w,h) in faces[0]:
             area = w*w
             if area == req_area:
-                new_x = int(x+w//2)
-                corrected_x = base*round(new_x/base) # rounding to the nearest base th multiple
-                new_y = int(y+h//2)
-                corrected_y = base*round(new_y/base)
+                fc_x = int(x+w//2)
+                fc_y = int(y+h//2)
 
-                corrected_deltax = abs(corrected_x-target_center[0])
-                corrected_deltay = abs(corrected_y-target_center[1])
-
-                if corrected_deltax >= delta or corrected_deltay >= delta:
-                    target_center = (corrected_x, corrected_y)
-
-                move =  (target_center[0]-actual_center[0], actual_center[1]-target_center[1])
+                move_x = screen_center[0]-fc_x
+                move_y = screen_center[1]-fc_y
 
 
-                if move[0] >= threshold:
-                    move_x(rev=-1, rapid=True)
-                elif move[0] <= -threshold:
-                    move_x(rapid=True)
+                if move_x >= threshold:
+                    #turn right
+                    drone.control('e')
+                elif move_x <= -threshold:
+                    #turn left
+                    drone.control('q')
                 else:
                     print("no x")
-                if move[1] >= threshold:
-                    move_y(rev=-1, rapid=True)
-                elif move[1] <= -threshold:
-                    move_y(rapid=True)
+
+                if move_y >= threshold:
+                    #go up 
+                    drone.control('r')
+                elif move_y <= -threshold:
+                    #go down
+                    drone.control('f')
                 else:
                     print("no y")
 
-            elif area < req_area:
-                move_z()
-            elif area > req_area:
-                move_z(-1)
+                if area < req_area:
+                    #go_forward()
+                    drone.control('w')
+                elif area > req_area:
+                    #go_backwards()
+                    drone.control('s')
+
 
             cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
             cv2.circle(img, target_center, 5,(255,0,0),2)
             cv2.circle(img, actual_center, 5,(0,255,0),2)
         cv2.imshow('img',img)
-    process=not process
+
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
